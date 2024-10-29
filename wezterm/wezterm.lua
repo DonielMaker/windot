81a7bab740e93c26b00f6f962ee523cdb359b8ca
donielmaker <daniel.schmidt0204@gmail.com>
2024-10-30 00:58:48 +0100

    Fixed wezterm asking for close confirmation

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,5 +5,6 @@ return {
     color_scheme = "Tokyo Night Storm",
     font = wezterm.font("FiraCode Nerd Font"),
     window_background_opacity = 0.8,
+    window_close_confimation = "Never",
     enable_tab_bar = false
 }
