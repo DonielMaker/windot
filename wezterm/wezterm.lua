460abbdea94694b6ae5470c15dc774bfddd93d50
donielmaker <daniel.schmidt0204@gmail.com>
2024-10-30 00:13:05 +0100

    Fixed wezterm better return

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,9 @@
 require('keybindings')
 local wezterm = require("wezterm")
 
-local config = wezterm.config_builder()
-
-config.color_scheme = "Tokyo Night Storm"
-config.window_background_opacity = 0.8
-config.font = wezterm.font("FiraCode Nerd Font")
-config.enable_tab_bar = false
-
-return config
+return {
+    color_scheme = "Tokyo Night Storm",
+    window_background_opacity = 0.8,
+    font = wezterm.font("FiraCode Nerd Font"),
+    enable_tab_bar = false
+}
